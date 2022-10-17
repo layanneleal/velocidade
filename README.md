95a0948ce2e35c7102e8b962498170d47637a40c
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-10-17 06:02:41 -0300

    Não finalizado

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # velocidade
- Programa verifica velocidade
+ **Programa velocímetro**  
+ Programa que ler a velocidade permitida em uma estrada e
+a velocidade de um condutor. Se a velocidade for inferior ou igual à
+permitida, exibe “Sem Multa”. Se a velocidade for de até 20% maior que a
+permitida, exibe “Multa Leve”. E, se a velocidade for superior a 20% da
+velocidade permitida, exibe “Multa Grave”.
---
+++ script.js
@@ -0,0 +1,11 @@
+function calcularVelocidade() {
+  //Cria referência aos elementos da página
+  let inVelocidadeP = document.getElementById('inVelocidadeP')
+  let inVelocidadeC = document.getElementById('inVelocidadeC')
+  let outResposta = document.getElementById('outResposta')
+
+  //Obtem conteúdos dos campos de entrada
+  let velocidadeP = Number(inVelocidadeP.value)
+  let velocidadeC = Number(inVelocidadeC.value)
+
+}
