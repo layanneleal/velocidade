45816ca2ae032d19db49d053ac85fff0c493888b
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-10-18 06:33:01 -0300

    Não finalizado

--- script.js
+++ script.js
@@ -5,7 +5,28 @@ function calcularVelocidade() {
   let outResposta = document.getElementById('outResposta')
 
   //Obtem conteúdos dos campos de entrada
-  let velocidadeP = Number(inVelocidadeP.value)
-  let velocidadeC = Number(inVelocidadeC.value)
+  let velP = Number(inVelocidadeP.value)
+  let velC = Number(inVelocidadeC.value)
+  let porc = velP * 0.2
 
+  if (velP == 0 && velC == 0 || isNaN(velP + velC)) {
+    alert('Digite uma velocidade válida')
+    velP.focus()
+    return
+  }
+
+  if (velC <= velP) {
+    outResposta.textContent = 'Sem multa'
+  }
+
+  if (velC + porc ) {
+    outResposta.textContent = 'Multa leve'
+  }
+   
+  if (velC > porc * 0.2 ) {
+    outResposta.textContent = 'Multa grave'
+  }
 }
+//Cria referência ao elemento btVerificar e associa a função ao click
+let btVerificar = document.getElementById('btVerificar')
+btVerificar.addEventListener('click', calcularVelocidade)
