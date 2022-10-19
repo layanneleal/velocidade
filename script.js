ae858dd051d4710dbd17ac1b975ec138c102bdc4
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-10-19 08:38:44 -0300

    Update

--- script.js
+++ script.js
@@ -9,25 +9,26 @@ function calcularVelocidade() {
   let velC = Number(inVelocidadeC.value)
   let res = 20 / 100 * velP
   
+  //Se velocidade permitida e do condutor for 0 ou NaN
   if (velP == 0 && velC == 0 || isNaN(velP + velC)) {
     alert('Digite uma velocidade válida')
     velP.focus()
     return
   }
-
+  //Se a velocidade do condutor for menor/igual à velocidade permitida
   if (velC <= velP) {
     outResposta.textContent = 'Sem multa'
   }
-
+  //Se a velocidade do condutor for maior e menor/igual a 20%
   if (velC > velP && velC <= res) {
     outResposta.textContent = 'Multa leve'
   }
-
-  if (velC > velP && velC > res) {
+  //Se a velocidade do condutor for maior que a permitida e maior que 20% da permitida
+  if (velC > velP && velC >= res) {
     outResposta.textContent = 'Multa grave'
   }
-  
 }
+  
 //Cria referência ao elemento btVerificar e associa a função ao click
 let btVerificar = document.getElementById('btVerificar')
 btVerificar.addEventListener('click', calcularVelocidade)
